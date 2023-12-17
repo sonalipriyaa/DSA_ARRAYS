e41bb6c082384de566117eb1efee7a3891d37152
sonalipriyaa <117668067+sonalipriyaa@users.noreply.github.com>
2023-12-17 17:10:37 +0530

    Add files via upload

---
+++ second_smallest.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a[]={1, 2, 4, 7, 7, 5};
+int n=6;
+int smallest=a[0];
+int second_smallest=INT_MAX;
+for(int i=0;i<n;i++){
+    if(a[i]<smallest){
+        second_smallest=smallest;
+        smallest=a[i];
+    }
+    else if(a[i]>smallest && a[i]<second_smallest){
+        second_smallest=a[i];
+    }
+}
+cout<<"second smallest element of array is : "<<second_smallest;
+    return 0;
+}
